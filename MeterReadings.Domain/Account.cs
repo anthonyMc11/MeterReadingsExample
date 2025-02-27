803a9e75e39691d7a302f54d26c86033a79c04ac
antmc <antmc11@gmail.com>
2025-02-27 13:12:36 +0000

    A1: created database items ready for next task

--- MeterReadings.Domain/Account.cs
+++ MeterReadings.Domain/Account.cs
@@ -1,7 +1,10 @@
-﻿namespace MeterReadings.Domain
-{
-    public class Account
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace MeterReadings.Domain;
+public class Account{
+    [Key]
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
     }
-}
