f724bf82c874595c7721a5ff4fcf01b3ffcaf2b9
antmc <antmc11@gmail.com>
2025-02-27 11:21:52 +0000

    B1: Create skeleton project with sql Server database configured and connected

---
+++ MeterReadings/Database/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace MeterReadings.Database;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {       
+    }  
+}
--- MeterReadings/Program.cs
+++ MeterReadings/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using MeterReadings.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                   options.UseSqlServer(builder.Configuration["ConnectionString"],
+                   sqlServerOptionsAction: sqlOptions =>
+                   {
+                       sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+                       //Configuring Connection Resiliency: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency 
+                       sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                   }));
 
 var app = builder.Build();
 
